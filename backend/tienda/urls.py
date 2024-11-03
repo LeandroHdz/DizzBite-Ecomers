3b9906bd158129f611f8f068e3c85d0b5d38973e
Leandro Hernández <leandrohernadezf@gmail.com>
2024-11-03 18:35:28 +0100

    creando ApiView para listado de product

--- backend/tienda/urls.py
+++ backend/tienda/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    
+    path('product/', ProductApiView.as_view(), name="product")
 ]
--- backend/tienda/views.py
+++ backend/tienda/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import permissions, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Product
+from .serializer import ProductSerializer
 
-# Create your views here.
+
+class ProductApiView(APIView):
+    # Permite el acceso a usuarios no autenticados
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request):
+        # Obtiene todos los productos de la base de datos
+        products = Product.objects.all()
+        # Serializa los datos de los productos
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        return Response(status=status.HTTP_403_FORBIDDEN)
+    def put(self, request):
+        return Response(status=status.HTTP_403_FORBIDDEN)
+    def delete(self, request):
+        return Response(status=status.HTTP_403_FORBIDDEN)
