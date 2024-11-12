4544fdfbb665293b8ad690c0da48f818d56cb4c3
Leandro Hernández <leandrohernadezf@gmail.com>
2024-11-12 20:30:17 +0100

    modificando PromoCard para pasarle las props

--- dizzbite-shop/src/app/page.tsx
+++ dizzbite-shop/src/app/page.tsx
@@ -10,66 +10,75 @@ import PromoCard from "@/components/mc/promoCard";
 
 
 const products = [
-  {   
-      id: 1,
-      image: '/promos/imgTest.png',
-      name: 'Apple Watch Series 7 GPS',
-      price: 599,
-      discount: 10, // 10% de descuento
+  {
+    id: 1,
+    image: '/promos/imgTest.png',
+    name: 'Apple Watch Series 7 GPS',
+    price: 599,
+    discount: 10, // 10% de descuento
   },
   {
-      id:2,
-      image: '/promos/imgTest1.png',
-      name: 'Apple Watch Series 7 GPS',
-      price: 599,
-      discount: 10, // 10% de descuento
-      rating:4
-  },
-  {   
-      id:3,
-      image: '/promos/imgTest2.jpeg',
-      name: 'Apple Watch Series 7 GPS',
-      price: 599,
-      discount: 10, // 10% de descuento
-      rating: 3
-  },
-  
-  { 
-      id:4,
-      image: '/promos/imgTest3.jpg',
-      name: 'Samsung Galaxy Buds Pro',
-      price: 199,
+    id: 2,
+    image: '/promos/imgTest1.png',
+    name: 'Apple Watch Series 7 GPS',
+    price: 599,
+    discount: 10, // 10% de descuento
+    rating: 4
   },
   {
-      id:5,
-      image: '/promos/imgTest4.png',
-      name: 'Sony WH-1000XM4 Headphones',
-      price: 349,
-      discount: 15, // 15% de descuento
+    id: 3,
+    image: '/promos/imgTest2.jpeg',
+    name: 'Apple Watch Series 7 GPS',
+    price: 599,
+    discount: 10, // 10% de descuento
+    rating: 3
   },
-  {   
-      id:6,
-      image: '/promos/imgTest5.jpg',
-      name: 'Fitbit Charge 5aasdasasds asd',
-      price: 179,
+
+  {
+    id: 4,
+    image: '/promos/imgTest3.jpg',
+    name: 'Samsung Galaxy Buds Pro',
+    price: 199,
+  },
+  {
+    id: 5,
+    image: '/promos/imgTest4.png',
+    name: 'Sony WH-1000XM4 Headphones',
+    price: 349,
+    discount: 15, // 15% de descuento
+  },
+  {
+    id: 6,
+    image: '/promos/imgTest5.jpg',
+    name: 'Fitbit Charge 5aasdasasds asd',
+    price: 179,
   },
 ];
 
 
+
+
+
 export default function Home() {
   return (
-      <>
-      <NavbarDesktop/>
-      <HeroSection/>
-      <CatPopulares/>
-      <ScrollProducts title="Más Vendidos" products={products}/>
-      <PromoCard/>
-      <ScrollProducts title="Productos Recientes" products={products}/>
-      <ScrollProducts title="Oferta y Promociones" products={products}/>
-      <PromoCard/>
-      <ScrollCard/> 
-      <ScrollProducts title="Recomendados" products={products}/>
-      <Footer/>
-      </>
+    <>
+      <NavbarDesktop />
+      <HeroSection />
+      <CatPopulares />
+      <ScrollProducts title="Más Vendidos" products={products} />
+      <PromoCard img="/test/vecteezy_photorealistic-product-shot-food-photography-ketc_45476337.jpg"
+        title='¡Oferta Exclusiva!'
+        description='Aprovecha un 20% de descuento en toda la tienda. Solo por tiempo limitado.'
+        btnText="Comprar Ahora" />
+      <ScrollProducts title="Productos Recientes" products={products} />
+      <ScrollProducts title="Oferta y Promociones" products={products} />
+      <PromoCard img="/test/jakub-kapusnak-Hj53USePB1E-unsplash.jpg"
+        title='¡Oferta Exclusiva!'
+        description='Aprovecha un 20% de descuento en toda la tienda. Solo por tiempo limitado.'
+        btnText="Comprar Ahora" />
+      <ScrollCard />
+      <ScrollProducts title="Recomendados" products={products} />
+      <Footer />
+    </>
   );
 }
--- dizzbite-shop/src/components/mc/promoCard.tsx
+++ dizzbite-shop/src/components/mc/promoCard.tsx
@@ -1,12 +1,22 @@
+import React from "react";
+
+interface PromoCardProps {
+  img: string;
+  title: string;
+  description:string;
+  btnText: string
+}
 
 
-const PromoCard = () => {
+const PromoCard: React.FC<PromoCardProps> = ({
+  img,title,description,btnText
+}) => {
   return (
     <div className="flex p-2 h-[300px]">
       {/* Imagen en la Izquierda */}
       <div className="lg:w-1/2">
         <img
-          src="/test/jakub-kapusnak-Hj53USePB1E-unsplash.jpg"
+          src={img}
           alt="Promoción Especial"
           className="w-full h-full object-cover rounded-l-xl"
         />
@@ -14,13 +24,13 @@ const PromoCard = () => {
        {/* Contenido a la Derecha */}
     <div className="lg:w-1/2 p-8 flex flex-col justify-center bg-gradient-to-r from-slate-950 to-purple-900 text-white rounded-r-xl">
       <div className="text-4xl font-extrabold mb-2 drop-shadow-md">
-        ¡Oferta Exclusiva!
+        {title}
       </div>
       <div className="text-lg mb-6 drop-shadow-md">
-        Aprovecha un <span className="font-bold">20% de descuento</span> en toda la tienda. Solo por tiempo limitado.
+          {description}
       </div>
       <button className="bg-white text-blue-600 px-6 py-3 rounded-full font-semibold shadow-md hover:shadow-lg hover:bg-gray-100 transition duration-200 ease-in-out transform hover:-translate-y-1">
-        Comprar Ahora
+        {btnText}
       </button>
     </div>
     </div>
